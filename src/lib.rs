4c84754c2b47541eaf6d7a369a56fdc22f0f8819
Emilio Ziniades <emilioziniades@protonmail.com>
2023-08-31 13:50:38 +0200

    first pass at documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
+#![doc = include_str!("../README.md")]
+
+/// TODO: documentation
 tonic::include_proto!("braiins.bos");
+
+/// TODO: documentation
 pub mod v1 {
     tonic::include_proto!("braiins.bos.v1");
 }
